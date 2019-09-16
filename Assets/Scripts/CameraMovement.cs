0ef5f52c46399010934c5229c8399d0e11563859
Asher Hennig [Student] <asher.hennig@student.sl.on.ca>
2019-09-16 16:02:40 -0400

    Added Player Controller, and Camera Mount Scripts

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    public GameObject followTarget;
+    public float moveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (followTarget != null)
+        {
+            transform.position = Vector3.Lerp(transform.position,
+                                            followTarget.transform.position,
+                                            Time.deltaTime * moveSpeed);
+        }
     }
 }
