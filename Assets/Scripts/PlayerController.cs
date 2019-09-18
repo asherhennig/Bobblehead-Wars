83440409dc6bd2cb8e9704f52143c2c9ebebd6ba
Asher Hennig [Student] <asher.hennig@student.sl.on.ca>
2019-09-18 15:50:51 -0400

    Added bobblehead functionality to head.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
+    public Rigidbody head;
+    public LayerMask layerMask;
+    private Vector3 currentLookTarget = Vector3.zero;
     private CharacterController characterController;
 
     // Start is called before the first frame update
@@ -19,5 +22,26 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
                                 0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
+                                            0, Input.GetAxis("Vertical"));
+
+        if (moveDirection == Vector3.zero)
+        {
+            //TODO
+        }
+        else
+        {
+            head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
+
+
     }
 }
