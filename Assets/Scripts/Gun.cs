4f0729026f46b9c455b08dc7324d25eba5e5ce26
Asher Hennig [Student] <asher.hennig@student.sl.on.ca>
2019-09-26 14:53:19 -0400

    added gun upgrade

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,19 +6,45 @@ public class Gun : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform launchPosition;
+    public bool isUpgraded;
+    public float upgradeTime = 10.0f;
+
+    private float currentTime;
 
     private AudioSource audioSource;
 
+    private Rigidbody createBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        bullet.transform.position = launchPosition.position;
+        return bullet.GetComponent<Rigidbody>();
+    }
+
     void fireBullet ()
     {
-        // 1    Sets Game Object to Bullet Prefab
-        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
-        // 2    Sets start position to front of gun barrel
-        bullet.transform.position = launchPosition.position;
-        // 3    Moves bullets
-        bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100;
+        Rigidbody bullet = createBullet();
+        bullet.velocity = transform.parent.forward * 100;
+        if (isUpgraded)
+        {
+            Rigidbody bullet2 = createBullet();
+            bullet2.velocity = (transform.right + transform.forward / 0.5f) * 100;
+            Rigidbody bullet3 = createBullet();
+            bullet3.velocity = ((transform.right * -1) + transform.forward / 0.5f) * 100;
+        }
+        if (isUpgraded)
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.upgradedGunFire);
+        }
+        else
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.gunFire);
+        }
+    }
 
-        audioSource.PlayOneShot(SoundManager.Instance.gunFire);
+    public void UpgradedGun()
+    {
+        isUpgraded = true;
+        currentTime = 0;
     }
 
     // Start is called before the first frame update
