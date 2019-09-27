17adf8a028d6cc6ddea9241fb99fb61f493e34c2
Asher Hennig [Student] <asher.hennig@student.sl.on.ca>
2019-09-27 15:57:25 -0400

    added character and alien deCaps

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -8,9 +8,12 @@ public class Alien : MonoBehaviour
 {
     public UnityEvent OnDestroy;
     public Transform target;        //Target for alien
+    public Rigidbody head;
+
     private NavMeshAgent agent;
 
     public float navigationUpdate;
+    public bool isAlive = true;
 
     private float navigationTime = 0;
 
@@ -23,13 +26,16 @@ public class Alien : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (target != null)
+        if (isAlive)
         {
-            navigationTime += Time.deltaTime;
-            if (navigationTime > navigationUpdate)
+            if (target != null)
             {
-                agent.destination = target.position;
-                navigationTime = 0;
+                navigationTime += Time.deltaTime;
+                if (navigationTime > navigationUpdate)
+                {
+                    agent.destination = target.position;
+                    navigationTime = 0;
+                }
             }
         }
 
@@ -37,14 +43,26 @@ public class Alien : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Die();     //Destroys alien on contact with selected game object.
-        SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        if (isAlive)
+        {
+            Die();                               //Destroys alien on contact with selected game object.
+            SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        }
     }
 
     public void Die()
     {
-        Destroy(gameObject);
+        isAlive = false;
+        head.GetComponent<Animator>().enabled = false;
+        head.isKinematic = false;
+        head.useGravity = true;
+        head.GetComponent<SphereCollider>().enabled = true;
+        head.gameObject.transform.parent = null;
+        head.velocity = new Vector3(0, 26.0f, 3.0f);
+
         OnDestroy.Invoke();
         OnDestroy.RemoveAllListeners();
+        SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float[] hitForce;
     public float moveSpeed = 50.0f;     //Movement speed for player
+    public float timeBetweenHits = 2.5f;
+
+    private int hitNumber = -1;
+    private float timeSinceHit = 0;
+    private bool isHit = false;
+    private bool isDead = false;
+
+    public Rigidbody marineBody;
     public Rigidbody head;
     public LayerMask layerMask;
     public Animator bodyAnimator;
@@ -24,6 +33,16 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
                                 0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
+
+        if (isHit)
+        {
+            timeSinceHit += Time.deltaTime;
+            if (timeSinceHit > timeBetweenHits)
+            {
+                isHit = false;
+                timeSinceHit = 0;
+            }
+        }
     }
 
     void FixedUpdate()
@@ -71,5 +90,50 @@ public class PlayerController : MonoBehaviour
         //3
         transform.rotation = Quaternion.Lerp(transform.rotation, rotation,
                                                 Time.deltaTime * 10.0f);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Alien alien = other.gameObject.GetComponent<Alien>();
+        if (alien != null)
+        {
+            //1
+            if (!isHit)
+            {
+                //2
+                hitNumber += 1;
+                CameraShake cameraShake = Camera.main.GetComponent<CameraShake>();
+                //3
+                if (hitNumber < hitForce.Length)
+                {
+                    cameraShake.intensity = hitForce[hitNumber];
+                    cameraShake.Shake();
+                }
+                else
+                {
+                    Die();
+                }
+                //4
+                isHit = true;
+                SoundManager.Instance.PlayOneShot(SoundManager.Instance.hurt);
+            }
+            alien.Die();
+        }
+    }
+
+    public void Die()
+    {
+        bodyAnimator.SetBool("IsMoving", false);
+        marineBody.transform.parent = null;
+        marineBody.isKinematic = false;
+        marineBody.useGravity = true;
+        marineBody.gameObject.GetComponent<CapsuleCollider>().enabled = true;
+        marineBody.gameObject.GetComponent<Gun>().enabled = false;
+
+        Destroy(head.gameObject.GetComponent<HingeJoint>());
+        head.transform.parent = null;
+        head.useGravity = true;
+        SoundManager.Instance.PlayOneShot(SoundManager.Instance.marineDeath);
+        Destroy(gameObject);
     }
 }
